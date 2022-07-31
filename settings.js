6c8243a16901eb39798d40c122564a6a9ee67b4b
AKKolla <110313661+AKKolla@users.noreply.github.com>
2022-07-31 06:12:13 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -21,19 +21,19 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['94763427338']
-global.premium = ['94763427338']
-global.ownernomer = '94763427338'
-global.ownername = '៚ᴠɪʜͥᴀɴͣɢͫᴀ ʏᴛ͢⁸⁵⁴¹⃟✦⃝̮̮〽️'
-global.botname = 'VIHANGA MD BOT'
-global.footer = 'VIHANGA MD'
-global.cap = '*Genarated By Vihanga Md*'
+global.owner = ['94740120962']
+global.premium = ['94740120962']
+global.ownernomer = '94740120962'
+global.ownername = '៚SL ACHIYA KOLLA'
+global.botname = 'ACHIYA MD BOT'
+global.footer = 'ACHIYA MD'
+global.cap = '*Genarated By Achiya Md*'
 global.ig = 'https://github.com/vihangabot'
 global.region = 'Sri Lanka, Sabaragamuwa, Embilipitiya'
 global.sc = 'https://github.com/vihangabot'
 global.myweb = 'https://chat.whatsapp.com/BDge3i6chrQKWkkSRellyT'
-global.packname = 'Vihanga Md Sticker Bot'
-global.author = '៚ᴠɪʜͥᴀɴͣɢͫᴀ ʏᴛ͢⁸⁵⁴¹⃟✦⃝̮̮〽️'
+global.packname = 'Achiya Md Sticker Bot'
+global.author = '៚SL ACHIYA'
 global.autobio = 'true'
 global.sessionName = 'session'
 global.prefa = ['','!','.','🐦','🐤','🗿']
